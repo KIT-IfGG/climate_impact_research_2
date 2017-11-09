73a3c9e10fcbc508e7d123b2a1c6f8e88ef02db6
kdolos <dolos@kit.edu>
2017-11-09 19:29:43 +0100

    instlal_packages update

--- r-code/install_packages.R
+++ r-code/install_packages.R
@@ -1,14 +1,19 @@
-
-### Install R- package ProfoundData
+### Install R- package ProfoundData ####
+### You must install all dependencies (e.g. zoo, RSQLite)!
 install.packages("../data/R-Packages/ProfoundData_0.1.6.tar.gz", build_vignettes = TRUE, repos = NULL)
 
 library(ProfoundData)
 ?ProfoundData
 vignette("ProfoundDataVignette", package="ProfoundData")
 
+### Download the PROFOUND data base ####
+### http://cost-profound.eu/cloud/public.php?service=files&t=6b0c165b76a8e8abdf5028286e78096e
 
-### Install R-Package LandClimTools
+### Install R-Package LandClimTools ####
 library(devtools)
 install_github("KIT-IfGG/LandClimTools")
 library(LandClimTools)
 ?LandClimTools
+
+
+
