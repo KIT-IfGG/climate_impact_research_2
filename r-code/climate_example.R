a8003c0e2a7c0320768f07965e09209779cb753a
kdolos <dolos@kit.edu>
2017-11-23 11:03:15 +0100

    climte example updated to new DB sturcture

--- r-code/climate_example.R
+++ r-code/climate_example.R
@@ -13,12 +13,11 @@ colnames(overview)
 clim <- ProfoundData::getData("CLIMATE_ISIMIPFT", "bily_kriz")
 str(clim, 1)
 
-climdata$date <- as.Date(climdata$date)
+clim$date <- as.Date(clim$date)
 
 table(clim$forcingConditions)
 table(clim$year)
 table(clim$forcingDataset)
-
 
 ### Global circulation models, z.B. 
 ## https://verc.enes.org/models/earthsystem-models/metoffice-hadley-centre/hadgem2-es
@@ -28,13 +27,18 @@ table(clim$forcingDataset)
 ### van Vuuren, D.P., Edmonds, J., Kainuma, M., Riahi, K., Thomson, A., Hibbard, K., Hurtt, G.C., Kram, T., Krey, V., Lamarque, J.-F., Masui, T., Meinshausen, M., Nakicenovic, N., Smith, S.J., & Rose, S.K. 2011. The representative concentration pathways: an overview. Climatic Change 109: 5–31.
 
 ### "Historical" is simulated climate data for the past, which fits to the "future" scenarios
-climdata <- clim[clim$forcingConditions=="historical" & clim$forcingDataset =="HadGEM2ES",]
-head(climdata)
-summary(climdata)
-plot(tmean_degC ~ date, climdata, type="l", col="red")
+clim_site <- clim[clim$forcingConditions=="historical" & clim$forcingDataset =="HadGEM2ES",]
+head(clim_site)
+summary(clim_site)
+
+plot(tmean_degC ~ date, data=clim_site, type="l", col="gray")
+abline(h=mean(clim_site$tmean_degC), lwd=2, col="gray")
+lines(smooth.spline(clim_site$date, clim_site$tmean_degC, nknots=50), lwd=2, col="tomato")
+abline(lm(tmean_degC ~ date, data=clim_site), lwd=3, col="tomato")
+
 
 ### LandClim climate data header, read in example
-header <- readLines("data/landclim_site_infos/climate_header.txt")
+header <- readLines("../data/landclim_site_infos/climate_header.txt")
 header[2]   ### Latitude must match latitude of site
 
 header[2] <- site_infos$lat[site_infos$name1=="BilyKriz"]
