2266e86f7b091f04583a93620e54d361137b9c17
kdolos <klara.dolos@gmail.com>
2018-02-01 10:45:57 +0100

    minor changes

--- r-code/install_packages.R
+++ r-code/install_packages.R
@@ -7,6 +7,7 @@
 # install.packages("rgdal", type = "source")
 install.packages("devtools")
 
+### Install the ProfoundData package from file ####
 # install.packages("../data/R-Packages/ProfoundData_0.1.6.tar.gz", build_vignettes = TRUE, repos = NULL)
 
 library(ProfoundData)
@@ -14,7 +15,7 @@ library(ProfoundData)
 vignette("ProfoundDataVignette", package="ProfoundData")
 
 ### Download the PROFOUND data base ####
-### http://cost-profound.eu/cloud/public.php?service=files&t=6b0c165b76a8e8abdf5028286e78096e
+### Use link provided in the course slides
 
 ### Install R-Package LandClimTools ####
 library(devtools)
