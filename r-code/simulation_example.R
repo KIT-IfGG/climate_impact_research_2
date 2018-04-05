aee5549deaf6857b7edafdb18951a22aad24358e
kdolos <klara.dolos@gmail.com>
2018-04-05 16:29:23 +0200

    added plot_gradient example

--- r-code/simulation_example.R
+++ r-code/simulation_example.R
@@ -31,23 +31,5 @@ file.copy("Input/species_actual.xml", "Input/species.xml", overwrite=TRUE)      
 
 ### Plot succession ####
 setwd(proj_wd)
-
-species_codes <- read.csv("data/landclim_site_infos/species_codes.csv", stringsAsFactors=F)
-species_codes$colors <- rainbow(nrow(species_codes))
-species_codes$colors <- colors()[(1:nrow(species_codes))*2]
-
-sim_species <- unique(as.character(unlist(lapply(sites_sim, function(x) read_species_xml(paste("simulations/", tolower(x), "/Input/species.xml", sep=""))$name))))
-sim_species <- data.frame(name=sim_species, colors=landclim_colors(length(sim_species)), stringsAsFactors=F)
-
-pie(rep(1, nrow(sim_species)), col=sim_species$colors)
-
-
-pdf("figures/succession_plots.pdf", width=7, height=4)
-for(i in 1:NROW(sites_sim)){
-  dat <- read.table(paste("simulations/", tolower(sites_sim[i]), "/Output/Output\\elevation_biomass_out.csv", sep=""), header=T, sep=",", dec=".")
-  mycols <- sim_species$colors[match(names(dat)[2:(ncol(dat)-2)], sim_species$name)]
-  plot_gradient(dat$decade, dat[,2:(ncol(dat)-2)], ylab="Biomass (t/ha)", xlab="Time (yrs)", main=sites_sim[i], ylim=c(0,500), col=mycols)
-  legend("bottomright", legend=names(dat)[2:(ncol(dat)-2)], col=mycols, pch=15, title="Species", bg="white")
-  
-}
-dev.off()
+dat <- read.table(paste("simulations/", tolower(sites_sim), "/Output/Output\\elevation_biomass_out.csv", sep=""), header=T, sep=",", dec=".")
+plot_gradient(dat$decade, dat[,2:(ncol(dat)-2)], ylab="Biomass (t/ha)", xlab="Time (yrs)", main=sites_sim, ylim=c(0,500))
